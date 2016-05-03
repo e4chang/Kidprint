7d96d8bb1cdf3e1f33c3a369468afdeb84e18f6c
Eric <e4chang@ucsd.edu>
2016-05-02 20:16:14 -0700

    Fixed direction bug

--- autofocus.py
+++ autofocus.py
@@ -9,7 +9,7 @@ class FocusState:
 	    self.rate = 0.0
 	    self.rateMax = 0.0
 	    self.lastSucceeded = True
-	    self.minFocusStep = 1
+	    self.minFocusStep = 4
 	    self.lastDirectionChange = 0
 
 #state = FocusState()
@@ -19,34 +19,44 @@ def correctFocus(rate, state):
 	# print state.direction
 	state.lastDirectionChange += 1
 	rateDelta = rate - state.rate
+	#print rate, state.rate
 
 	if rate > state.rateMax + epsylon:
 		state.stepToLastMax = 0
 		state.rateMax = rate
 		lastDirectionChange = 0
+		#print "dir change"
 
 	if not state.lastSucceeded:
 		state.direction *= -1
 		state.lastDirectionChange = 0
 		state.step = state.step / 2
 	else:
+		#print rate + epsylon,"<", state.rateMax,state.lastDirectionChange,"\n"
+		#print rateDelta, "<", -epsylon
 		if rate < epsylon:
 			state.step = FOCUS_STEP
 		elif rateDelta < -epsylon:
 			state.direction *= -1
-			state.step = state.step * 0.75
+			#print "dir change"
+			state.step = state.step * 3 / 4
 			state.lastDirectionChange = 0
-		elif (rate + epsylon < state.rateMax) and (state.lastDirectionChange > 3) or (state.step < (state.minFocusStep * 1.5) and state.stepToLastMax > state.step):
-			state.direction = 1 if state.stepToLastMax >= 1 else -1
-			state.step = state.step * 0.75
+		elif ((rate + epsylon < state.rateMax) and
+		     ((state.lastDirectionChange > 3) or
+         (state.step < (state.minFocusStep * 3 / 2) and
+         state.stepToLastMax > state.step))):
+			state.direction = 1 if state.stepToLastMax >= 0 else -1
+			state.step = state.step * 3 / 4
 			stepToMax = state.stepToLastMax
 			state.stepToLastMax = 0
 			state.lastDirectionChange = 0
 			state.rate = rate
-			#print 2
+			#print "dir change"
+			#print stepToMax, state.direction, state.stepToLastMax
 			return stepToMax
 	#print lastDirectionChange
 	state.rate = rate
-	state.step = state.direction * state.step
-	state.stepToLastMax -= state.step
-	return state.step
+	tempStep = state.direction * state.step
+	state.stepToLastMax -= tempStep
+	#print tempStep, state.direction, state.stepToLastMax
+	return tempStep
--- test_autofocus.py
+++ test_autofocus.py
@@ -1,8 +1,9 @@
 import autofocus
 import math
 import random
+import matplotlib.pyplot as plt
 
-NOISE = 0.005
+NOISE = 0.05
 MAX_FOCUS = 1024
 
 def noise():
@@ -14,19 +15,29 @@ def generateWave(offset):
 	return [math.sin(math.pi*(i-offset)/MAX_FOCUS) + noise() for i in range(MAX_FOCUS)]
 
 def testAutofocus():
-    global lastSucceeded
     focusPoint = 0
     state = autofocus.FocusState()
-    sinFunc = generateWave(200)
-    for i in range(20):
-        lastSucceeded = focusPoint < len(sinFunc) and focusPoint >= 0
+    sinFunc = generateWave(300)
+    #print sinFunc
+    foci = [0,0]
+    for i in range(43):
+        state.lastSucceeded = focusPoint < len(sinFunc) and focusPoint >= 0
         stepVal = autofocus.correctFocus(sinFunc[int(focusPoint)], state)
-        print int(stepVal), sinFunc[int(focusPoint)], '\n'
+        #print int(stepVal), sinFunc[int(focusPoint)], '\n'
         focusPoint = focusPoint + stepVal
         focusPoint = 0 if focusPoint < 0 else focusPoint
         focusPoint = len(sinFunc)-1 if focusPoint >= len(sinFunc) else focusPoint
-        print state.stepToLastMax, focusPoint
+        foci.append(int(focusPoint))
+        #print state.stepToLastMax, focusPoint
 
+    fociVal = []
+    for f in foci:
+        fociVal.append(sinFunc[f])
+
+    plt.plot(sinFunc, 'r.', foci, fociVal, 'b^', foci[-1], fociVal[-1], 'gs')
+    plt.xlabel('Lens distance')
+    plt.ylabel('Focus rating')
+    plt.show()
 #state = autofocus.FocusState()
 #print state.direction
 #print autofocus.correctFocus(0.0)
